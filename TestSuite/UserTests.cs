f0ce2d6d47a142960fd43f95a074e7c6d97e94a0
JoakimIEngstrom <joakim.engstrom@utb.ecutbildning.se>
2021-11-23 14:58:06 +0100

    [UserTests]
    Har gjort tester på båda user metoderna.

--- TestSuite/UserTests.cs
+++ TestSuite/UserTests.cs
@@ -1,13 +1,60 @@
-﻿using Xunit;
+﻿using System.Collections.Generic;
+using System.Linq;
+using DataLayer.Backend;
+using DataLayer.Data;
+using DataLayer.Model;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
 
 namespace TestSuite
 {
     public class UserTests
     {
         [Fact]
-        public void Test1()
+        public void TestToReadAllUnsoldFoodboxes()
         {
+            AdminBackend adminBackend = new AdminBackend();
+            UserBackend userBackend = new UserBackend();
+
+            adminBackend.CreateAndSeedDb();
+
+            List<FoodBox> test = userBackend.UnsoldFoodBoxes("Fish");
+
+            Assert.Null(test[0].customer);
+            Assert.Null(test[1].customer);
+            Assert.Null(test[2].customer);
+
+            List<FoodBox> meatTest = userBackend.UnsoldFoodBoxes("Meat");
+
+            Assert.Equal("Köttfärssås", meatTest[0].Name);
+        }
+
+        [Fact]
+        public void TestToByFoodBox()
+        {
+            AdminBackend adminBackend = new AdminBackend();
+            UserBackend userBackend = new UserBackend();
+
+            adminBackend.CreateAndSeedDb();
+
+            using var ctx = new FoodResQCtx();
+            
+            var foodBox = ctx.Foodboxes.Find(3);
+            Assert.Null(foodBox.customer);
+            
+            userBackend.BuyFoodBox(3,1);
+            
+            using var ctx2 = new FoodResQCtx(); //Gör en ny koppling med den uppdaterade databasen.
+
+            var query = ctx2.Foodboxes
+                .Include(e => e.customer)
+                .Where(e => e.ID == 3);
+
+            foodBox = query.FirstOrDefault();
+            Assert.Equal(1, foodBox.customer.ID);
 
         }
     }
 }
+
+        
