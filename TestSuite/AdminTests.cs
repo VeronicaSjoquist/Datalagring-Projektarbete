8fcb2996e2f42b6b8faea715a2d0650c318b81e5
Veronica Sjöquist <veronica.sjoquist@utb.ecutbildning.se>
2021-11-23 12:13:36 +0100

    TestCreateAndSeed klar

--- TestSuite/AdminTests.cs
+++ TestSuite/AdminTests.cs
@@ -1,11 +1,37 @@
+using DataLayer.Backend;
+using DataLayer.Data;
 using Xunit;
 
 namespace TestSuite
 {
     public class AdminTests
     {
+        private AdminBackend admin;
+
+        public AdminTests()
+        {
+            admin = new AdminBackend();
+        }
+
         [Fact]
-        public void Test1()
+        public void TestCreateAndSeed()
+        {
+            admin.CreateAndSeedDb();
+
+            using (var ctx = new FoodResQCtx())
+            {
+                var customers = ctx.Customers;
+                var restaurants = ctx.Restaurants;
+                var foodboxes = ctx.Foodboxes;
+
+                Assert.NotEmpty(customers);
+                Assert.NotEmpty(restaurants);
+                Assert.NotEmpty(foodboxes);
+            }
+        }
+
+        [Fact]
+        public void TestShowAllRestaurants()
         {
 
         }
