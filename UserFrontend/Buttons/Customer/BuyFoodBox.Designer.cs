6b88efb5ee7f446b3d6621506914c5954e5fdfe2
JoakimIEngstrom <joakim.engstrom@utb.ecutbildning.se>
2021-11-29 12:42:30 +0100

    [Adderat ett par user Metoder]
    [Löst att köpa en box i Front end]
    
    Obs inte så snyggt.

--- UserFrontend/Buttons/Customer/BuyFoodBox.Designer.cs
+++ UserFrontend/Buttons/Customer/BuyFoodBox.Designer.cs
@@ -29,6 +29,24 @@
         private void InitializeComponent()
         {
             this.btnReturnFromBuyFoodBox = new System.Windows.Forms.Button();
+            this.btnVeronicasKöttfärssås = new System.Windows.Forms.Button();
+            this.btnKanelbulleKim = new System.Windows.Forms.Button();
+            this.listUnsold = new System.Windows.Forms.ListView();
+            this.ID = new System.Windows.Forms.ColumnHeader();
+            this.Food = new System.Windows.Forms.ColumnHeader();
+            this.Restaurant = new System.Windows.Forms.ColumnHeader();
+            this.Price = new System.Windows.Forms.ColumnHeader();
+            this.btnTheosRiceHouse = new System.Windows.Forms.Button();
+            this.listCustomers = new System.Windows.Forms.ListView();
+            this.cID = new System.Windows.Forms.ColumnHeader();
+            this.Customer = new System.Windows.Forms.ColumnHeader();
+            this.btnCustomers = new System.Windows.Forms.Button();
+            this.labelBuyBox = new System.Windows.Forms.Label();
+            this.labelCustomer = new System.Windows.Forms.Label();
+            this.labelBox = new System.Windows.Forms.Label();
+            this.textBoxCustomer = new System.Windows.Forms.TextBox();
+            this.textBoxBox = new System.Windows.Forms.TextBox();
+            this.btnBuy = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnReturnFromBuyFoodBox
@@ -43,23 +61,226 @@
             this.btnReturnFromBuyFoodBox.UseVisualStyleBackColor = true;
             this.btnReturnFromBuyFoodBox.Click += new System.EventHandler(this.btnReturnFromBuyFoodBox_Click);
             // 
+            // btnVeronicasKöttfärssås
+            // 
+            this.btnVeronicasKöttfärssås.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Purple;
+            this.btnVeronicasKöttfärssås.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnVeronicasKöttfärssås.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnVeronicasKöttfärssås.Location = new System.Drawing.Point(359, 66);
+            this.btnVeronicasKöttfärssås.Name = "btnVeronicasKöttfärssås";
+            this.btnVeronicasKöttfärssås.Size = new System.Drawing.Size(156, 83);
+            this.btnVeronicasKöttfärssås.TabIndex = 12;
+            this.btnVeronicasKöttfärssås.Text = "View Veronicas Köttfärssås";
+            this.btnVeronicasKöttfärssås.UseVisualStyleBackColor = true;
+            this.btnVeronicasKöttfärssås.Click += new System.EventHandler(this.btnVeronicasKöttfärssås_Click);
+            // 
+            // btnKanelbulleKim
+            // 
+            this.btnKanelbulleKim.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Purple;
+            this.btnKanelbulleKim.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnKanelbulleKim.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnKanelbulleKim.Location = new System.Drawing.Point(191, 66);
+            this.btnKanelbulleKim.Name = "btnKanelbulleKim";
+            this.btnKanelbulleKim.Size = new System.Drawing.Size(156, 83);
+            this.btnKanelbulleKim.TabIndex = 11;
+            this.btnKanelbulleKim.Text = "View Kanelbulle Kim";
+            this.btnKanelbulleKim.UseVisualStyleBackColor = true;
+            this.btnKanelbulleKim.Click += new System.EventHandler(this.btnKanelbulleKim_Click);
+            // 
+            // listUnsold
+            // 
+            this.listUnsold.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ID,
+            this.Food,
+            this.Restaurant,
+            this.Price});
+            this.listUnsold.Location = new System.Drawing.Point(20, 166);
+            this.listUnsold.Name = "listUnsold";
+            this.listUnsold.Size = new System.Drawing.Size(495, 176);
+            this.listUnsold.TabIndex = 10;
+            this.listUnsold.UseCompatibleStateImageBehavior = false;
+            this.listUnsold.View = System.Windows.Forms.View.Details;
+            // 
+            // ID
+            // 
+            this.ID.Text = "ID";
+            this.ID.Width = 40;
+            // 
+            // Food
+            // 
+            this.Food.Text = "Food";
+            this.Food.Width = 150;
+            // 
+            // Restaurant
+            // 
+            this.Restaurant.Text = "Restaurant";
+            this.Restaurant.Width = 200;
+            // 
+            // Price
+            // 
+            this.Price.Text = "Price";
+            this.Price.Width = 150;
+            // 
+            // btnTheosRiceHouse
+            // 
+            this.btnTheosRiceHouse.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Purple;
+            this.btnTheosRiceHouse.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnTheosRiceHouse.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnTheosRiceHouse.Location = new System.Drawing.Point(20, 66);
+            this.btnTheosRiceHouse.Name = "btnTheosRiceHouse";
+            this.btnTheosRiceHouse.Size = new System.Drawing.Size(156, 83);
+            this.btnTheosRiceHouse.TabIndex = 9;
+            this.btnTheosRiceHouse.Text = "View Theos Ricehouse";
+            this.btnTheosRiceHouse.UseVisualStyleBackColor = true;
+            this.btnTheosRiceHouse.Click += new System.EventHandler(this.btnUnsoldFoodFishBoxes_Click);
+            // 
+            // listCustomers
+            // 
+            this.listCustomers.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.cID,
+            this.Customer});
+            this.listCustomers.Location = new System.Drawing.Point(521, 166);
+            this.listCustomers.Name = "listCustomers";
+            this.listCustomers.Size = new System.Drawing.Size(184, 176);
+            this.listCustomers.TabIndex = 13;
+            this.listCustomers.UseCompatibleStateImageBehavior = false;
+            this.listCustomers.View = System.Windows.Forms.View.Details;
+            // 
+            // cID
+            // 
+            this.cID.Text = "ID";
+            this.cID.Width = 40;
+            // 
+            // Customer
+            // 
+            this.Customer.Text = "Customer";
+            this.Customer.Width = 150;
+            // 
+            // btnCustomers
+            // 
+            this.btnCustomers.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Purple;
+            this.btnCustomers.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnCustomers.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCustomers.Location = new System.Drawing.Point(521, 66);
+            this.btnCustomers.Name = "btnCustomers";
+            this.btnCustomers.Size = new System.Drawing.Size(184, 83);
+            this.btnCustomers.TabIndex = 14;
+            this.btnCustomers.Text = "View Customers";
+            this.btnCustomers.UseVisualStyleBackColor = true;
+            this.btnCustomers.Click += new System.EventHandler(this.btnCustomers_Click);
+            // 
+            // labelBuyBox
+            // 
+            this.labelBuyBox.AutoSize = true;
+            this.labelBuyBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelBuyBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelBuyBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.labelBuyBox.Location = new System.Drawing.Point(20, 355);
+            this.labelBuyBox.Name = "labelBuyBox";
+            this.labelBuyBox.Size = new System.Drawing.Size(360, 30);
+            this.labelBuyBox.TabIndex = 15;
+            this.labelBuyBox.Text = "Enter your ID and what you wish to buy.";
+            // 
+            // labelCustomer
+            // 
+            this.labelCustomer.AutoSize = true;
+            this.labelCustomer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelCustomer.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelCustomer.ForeColor = System.Drawing.SystemColors.Window;
+            this.labelCustomer.Location = new System.Drawing.Point(20, 386);
+            this.labelCustomer.Name = "labelCustomer";
+            this.labelCustomer.Size = new System.Drawing.Size(122, 30);
+            this.labelCustomer.TabIndex = 16;
+            this.labelCustomer.Text = "Customer ID";
+            // 
+            // labelBox
+            // 
+            this.labelBox.AutoSize = true;
+            this.labelBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.labelBox.Location = new System.Drawing.Point(20, 419);
+            this.labelBox.Name = "labelBox";
+            this.labelBox.Size = new System.Drawing.Size(70, 30);
+            this.labelBox.TabIndex = 17;
+            this.labelBox.Text = "Box ID";
+            // 
+            // textBoxCustomer
+            // 
+            this.textBoxCustomer.Location = new System.Drawing.Point(191, 390);
+            this.textBoxCustomer.Name = "textBoxCustomer";
+            this.textBoxCustomer.Size = new System.Drawing.Size(55, 27);
+            this.textBoxCustomer.TabIndex = 18;
+            // 
+            // textBoxBox
+            // 
+            this.textBoxBox.Location = new System.Drawing.Point(191, 423);
+            this.textBoxBox.Name = "textBoxBox";
+            this.textBoxBox.Size = new System.Drawing.Size(55, 27);
+            this.textBoxBox.TabIndex = 19;
+            // 
+            // btnBuy
+            // 
+            this.btnBuy.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Purple;
+            this.btnBuy.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnBuy.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnBuy.Location = new System.Drawing.Point(386, 355);
+            this.btnBuy.Name = "btnBuy";
+            this.btnBuy.Size = new System.Drawing.Size(129, 94);
+            this.btnBuy.TabIndex = 20;
+            this.btnBuy.Text = "Buy Box";
+            this.btnBuy.UseVisualStyleBackColor = true;
+            this.btnBuy.Click += new System.EventHandler(this.btnBuy_Click);
+            // 
             // BuyFoodBox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.DarkOliveGreen;
             this.ClientSize = new System.Drawing.Size(717, 553);
+            this.Controls.Add(this.btnBuy);
+            this.Controls.Add(this.textBoxBox);
+            this.Controls.Add(this.textBoxCustomer);
+            this.Controls.Add(this.labelBox);
+            this.Controls.Add(this.labelCustomer);
+            this.Controls.Add(this.labelBuyBox);
+            this.Controls.Add(this.btnCustomers);
+            this.Controls.Add(this.listCustomers);
+            this.Controls.Add(this.btnVeronicasKöttfärssås);
+            this.Controls.Add(this.btnKanelbulleKim);
+            this.Controls.Add(this.listUnsold);
+            this.Controls.Add(this.btnTheosRiceHouse);
             this.Controls.Add(this.btnReturnFromBuyFoodBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MinimumSize = new System.Drawing.Size(717, 553);
             this.Name = "BuyFoodBox";
             this.Text = "BuyFoodBox";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Button btnReturnFromBuyFoodBox;
+        private Button btnVeronicasKöttfärssås;
+        private Button btnKanelbulleKim;
+        private ListView listUnsold;
+        private ColumnHeader Food;
+        private ColumnHeader Restaurant;
+        private ColumnHeader Price;
+        private Button btnTheosRiceHouse;
+        private ColumnHeader ID;
+        private ListView listCustomers;
+        private ColumnHeader cID;
+        private ColumnHeader Customer;
+        private Button btnCustomers;
+        private Label labelBuyBox;
+        private Label labelCustomer;
+        private Label labelBox;
+        private TextBox textBoxCustomer;
+        private TextBox textBoxBox;
+        private Button btnBuy;
     }
 }
